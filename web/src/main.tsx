8cb15915b5e227e8d83038c6d107dcbbe041ac0f
Maicon Gabriel Friedel <maicon.friedel@grupokyly.com.br>
2024-09-12 00:40:51 -0300

    feat: added http requests to frontend and finished application

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  // <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>
+  </QueryClientProvider>
+  // </StrictMode>
 );
